a4037c692339c8250f790682e1389bee785c79ef
Harland Hauger <awkwardmelon@gmail.com>
2014-03-29 17:11:31 -0700

    brain can compute odds of hitting flush draw after flop

--- lib/brain.rb
+++ lib/brain.rb
@@ -1,8 +1,66 @@
 class Brain
 
-	# attr_reader :situation
+	attr_reader :situation, :hand_types, :outs, :odds
 
-	# def initialize
+	def draw(hand, deal)
+		@hand_types = { 1 => 'high_card', 2 => 'pair', 3 => 'two_pair', 4 => 'trips',
+		  5 => 'straight', 6 => 'flush', 7 => 'boat', 8 => 'quads', 9 => 'straight_flush' }
+		remaining = cards_left(deal)
+		@outs = {}
+		@odds = {}
+		@hand_types.each do |key, value|
+			@outs["#{value}"] = outs_calculator(hand, value)
+		end
+		# the results of the following line are the odds of hitting a flush by the river
+		@odds['flush'] = flush_draw(hand, @outs['flush'])
+
+		### Other things that this method might want:
+		# percent of what card comes out next
+		# the rest of the method calls(passed outs) to get all percentages
+	end
+
+	def flush_draw(hand, outs)
+		deck = (52 - hand.hand.hand.length).to_f
+		(@outs['flush'] / deck) + (((@outs['flush'] / (deck - 1)) * (1 - @outs['flush'] / deck)))
+	end
+
+	def outs_calculator(current_hand, desired_hand)
+		case desired_hand
+		when 'flush'
+			13 - count_suit(current_hand)
+		end
+	end
+
+	def cards_left(deal)
+		case deal
+		when 'preflop'
+			5
+		when 'flop'
+			2
+		when 'turn'
+			1
+		else 
+			nil
+		end
+	end
+
+	def count_suit(hand)
+		suit_count = 0
+		suits = %w{ H S C D }
+		suits.each do |s|
+			count = hand.hand.hand.count { |card| card.suit == s }
+			if count > suit_count 
+				suit_count = count
+			end
+		end
+		suit_count
+	end
+
+###################################################
+#  Big Future Projects/Considerations             #
+###################################################
+	# def read
+	# 	# accesses database info on player in question for a type
 	# end
 
 	# def situation
@@ -18,33 +76,6 @@ class Brain
 
 	# def pot_odds
 	# 	#computes 
-	# end
-
-	def draw(hand, deal)
-		flush_draw = count_suit(hand)
-		if flush_draw == 4 && deal == 'flop'
-			return ['flush', 0.35]
-		end
-		# current cards available
-		# place in hand (preflop, after turn, etc.)
-		# percent of what card comes out next
-		# chance of making hand
-	end
-
-	def count_suit(hand)
-		suit_count = 0
-		suits = %w{ H S C D }
-		suits.each do |s|
-			count = hand.hand.hand.count { |card| card.suit == s }
-			if count > suit_count 
-				suit_count = count
-			end
-		end
-		suit_count
-	end
-
-	# def read
-	# 	# accesses database info on player in question for a type
 	# end
 
 end
--- spec/brain_spec.rb
+++ spec/brain_spec.rb
@@ -9,14 +9,16 @@ describe Brain do
 			board_card1 = Card.new('H', 5)
 			board_card2 = Card.new('H', 2)
 			board_card3 = Card.new('C', 13)
-			test_player = test_game.table.players[0]
-			test_player.hole_cards << hole_card1
-			test_player.hole_cards << hole_card2
-			test_game.table.board << board_card1
-			test_game.table.board << board_card2
-			test_game.table.board << board_card3
-			test_hand = Hand.new(test_player.combine(test_game.table.board), test_player)
-			test_player.brain.draw(test_hand, 'flop').should eq ['flush', 0.35]
+			player = test_game.table.players[0]
+			board = test_game.table.board
+			player.hole_cards << hole_card1
+			player.hole_cards << hole_card2
+			board << board_card1
+			board << board_card2
+			board << board_card3
+			hand = Hand.new(player.combine(board), player)
+			player.brain.draw(hand, 'flop')
+			player.brain.odds['flush'].should be_within(0.005).of(0.3496)
 		end
 	end
 end
